c3de37f474c3550fde1ba7402b0fad5effe9cd46
Meghraj Falle <meghraj.falle@walmart.com>
2021-07-07 12:26:09 +0530

    Adding humidity in forecast description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -22,7 +22,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree celsius out. It feels like ' + body.current.feelslike + ' degrees out. There is a ' + body.current.precip + '% chance of rain.');
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degree celsius out. It feels like ' + body.current.feelslike + ' degrees out. There is a ' + body.current.precip + '% chance of rain. The humidity is '+ body.current.humidity+'%.');
         }
     })
 }
